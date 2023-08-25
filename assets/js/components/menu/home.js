151394bc3dee2a83f84779f4ed64d27d89ee33ab
LJTOUR VIAGENS E TURISMO <joaquimmigueldecristo@gmail.com>
2023-08-25 23:27:35 +0100

    first commit 4

--- assets/js/components/menu/home.js
+++ assets/js/components/menu/home.js
@@ -4,7 +4,12 @@ export default {
     data: function () {
         return {
             title: "home",
-            activo: false,
+            activo: false, 
+            activo1: true,
+            activo2: false,
+            activo3: false,
+            activo4: false,
+            activo5: false,
         }
     },
 
@@ -13,8 +18,49 @@ export default {
         mostrarMenu() {  
             this.activo = !this.activo; 
         },
-
       
+ 
+        homeMenu() { 
+                this.activo5 = false,
+                this.activo4 = false,
+                this.activo3 = false,
+                this.activo2 = false,
+                this.activo1 = true
+        },
+
+        sobreMenu() {
+            this.activo5 = false,
+            this.activo4 = false,
+            this.activo3 = false,
+            this.activo2 = true,
+            this.activo1 = false
+        },
+
+        servicoMenu() {
+            this.activo5 = false,
+            this.activo4 = false,
+            this.activo3 = true,
+            this.activo2 = false,
+            this.activo1 = false
+        },
+
+        galeriaMenu() {
+            this.activo5 = false,
+            this.activo4 = true,
+            this.activo3 = false,
+            this.activo2 = false,
+            this.activo1 = false
+        },
+
+        contactoMenu() {
+            this.activo5 = true,
+            this.activo4 = false,
+            this.activo3 = false,
+            this.activo2 = false,
+            this.activo1 = false
+        },
+
+         
       
     },
     template: await get_template('./assets/js/components/menu/home')
--- assets/js/view/home/home.js
+++ assets/js/view/home/home.js
@@ -1,56 +1,132 @@
 import get_template from '../../components/get_template.js'
 
 export default {
-    data: function () {
-        return {
-            title: "home"
-        } 
-        
-    },
+  data: function () {
+    return {
+      title: "home"
+    }
 
-    methods:{
-         
-    },
+  },
 
-    async mounted() {
+  methods: {
 
-        var slider = new KeenSlider(
-        "#my-keen-slider",
-        {
-          loop: true,
-        },
-        [
-          (slider) => {
-            let timeout
-            let mouseOver = false
-            function clearNextTimeout() {
-              clearTimeout(timeout)
-            }
-            function nextTimeout() {
-              clearTimeout(timeout)
-              if (mouseOver) return
-              timeout = setTimeout(() => {
-                slider.next()
-              }, 2000)
-            }
-            slider.on("created", () => {
-              slider.container.addEventListener("mouseover", () => {
-                mouseOver = true
-                clearNextTimeout()
-              })
-              slider.container.addEventListener("mouseout", () => {
-                mouseOver = false
-                nextTimeout()
-              })
+  },
+
+  async mounted() {
+
+    var slider = new KeenSlider(
+      "#my-keen-slider",
+      {
+        loop: true,
+      },
+      [
+        (slider) => {
+          let timeout
+          let mouseOver = false
+          function clearNextTimeout() {
+            clearTimeout(timeout)
+          }
+          function nextTimeout() {
+            clearTimeout(timeout)
+            if (mouseOver) return
+            timeout = setTimeout(() => {
+              slider.next()
+            }, 5000)
+          }
+          slider.on("created", () => {
+            slider.container.addEventListener("mouseover", () => {
+              mouseOver = true
+              clearNextTimeout()
+            })
+            slider.container.addEventListener("mouseout", () => {
+              mouseOver = false
               nextTimeout()
             })
-            slider.on("dragStarted", clearNextTimeout)
-            slider.on("animationEnded", nextTimeout)
-            slider.on("updated", nextTimeout)
-          },
-        ]
-      )
-        
-    },
-    template: await get_template('./assets/js/view/home/home')
+            nextTimeout()
+          })
+          slider.on("dragStarted", clearNextTimeout)
+          slider.on("animationEnded", nextTimeout)
+          slider.on("updated", nextTimeout)
+        },
+      ]
+    )
+
+
+
+    var slider = new KeenSlider(
+      "#my-keen-slider2",
+      {
+        loop: true,
+      },
+      [
+        (slider) => {
+          let timeout
+          let mouseOver = false
+          function clearNextTimeout() {
+            clearTimeout(timeout)
+          }
+          function nextTimeout() {
+            clearTimeout(timeout)
+            if (mouseOver) return
+            timeout = setTimeout(() => {
+              slider.next()
+            }, 2000)
+          }
+          slider.on("created", () => {
+            slider.container.addEventListener("mouseover", () => {
+              mouseOver = true
+              clearNextTimeout()
+            })
+            slider.container.addEventListener("mouseout", () => {
+              mouseOver = false
+              nextTimeout()
+            })
+            nextTimeout()
+          })
+          slider.on("dragStarted", clearNextTimeout)
+          slider.on("animationEnded", nextTimeout)
+          slider.on("updated", nextTimeout)
+        },
+      ]
+    )
+
+    var slider = new KeenSlider(
+      "#my-keen-slider3",
+      {
+        loop: true,
+      },
+      [
+        (slider) => {
+          let timeout
+          let mouseOver = false
+          function clearNextTimeout() {
+            clearTimeout(timeout)
+          }
+          function nextTimeout() {
+            clearTimeout(timeout)
+            if (mouseOver) return
+            timeout = setTimeout(() => {
+              slider.next()
+            }, 2000)
+          }
+          slider.on("created", () => {
+            slider.container.addEventListener("mouseover", () => {
+              mouseOver = true
+              clearNextTimeout()
+            })
+            slider.container.addEventListener("mouseout", () => {
+              mouseOver = false
+              nextTimeout()
+            })
+            nextTimeout()
+          })
+          slider.on("dragStarted", clearNextTimeout)
+          slider.on("animationEnded", nextTimeout)
+          slider.on("updated", nextTimeout)
+        },
+      ]
+    )
+
+  },
+  template: await get_template('./assets/js/view/home/home')
 }
